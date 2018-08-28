98b4683c5ef21161b771d399e626d9b017d31606
takehiro <takehiro.watanabe@globis.co.jp>
2018-08-28 16:16:33 +0900

    Initial Commit

---
+++ easycall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easycall
+//
+//  Created by Watanabe Takehiro on 2018/08/28.
+//  Copyright © 2018年 Watanabe Takehiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
